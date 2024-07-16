911bda2213213c52c1632c1815110ee02d75e0bd
Salma-Yousry <salmayousry779@gmail.com>
2024-07-16 20:34:36 +0300

    Revert "Feature/home screen"

--- lib/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-
-class AppColor{
-static const Color backcolor = Color(0xff242424);
-static const Color whitecolor = Color(0xffffffff);
-static const Color PrimeryLightColor = Color(0xffB7935F);
-}
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,69 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:islami_app/app_colors.dart';
-import 'package:islami_app/home/hadeth/hadeth_tab.dart';
-import 'package:islami_app/home/quran/quran_tab.dart';
-import 'package:islami_app/home/radio/radio_tab.dart';
-import 'package:islami_app/home/sebha/sebha_tab.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   static const String routeName = 'homeScreen';
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-int selectedindex =0;
-
-  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset("assets/images/background.png",
-          width: double.infinity,
-          height: double.infinity,
-          fit: BoxFit.fill,
-        ),
- Scaffold(
-  appBar: AppBar(
-title: Text('Islami',
-style:Theme.of(context).textTheme.bodyLarge
-),
-  ),
-   bottomNavigationBar: Theme(
-     data: Theme.of(context).copyWith(
-       canvasColor: Theme.of(context).primaryColor
-     ),
-     child: BottomNavigationBar(
-       currentIndex: selectedindex,
-       onTap: (index){
-         selectedindex=index;
-         setState(() {
-
-         });
-       },
-       items: [
-         BottomNavigationBarItem(icon:ImageIcon(AssetImage('assets/images/quran.png'),),
-             label: 'Quran'
-         ),
-         BottomNavigationBarItem(icon:ImageIcon(AssetImage('assets/images/quran_book.png'))
-         ,label: 'Hadeth'
-         ),
-         BottomNavigationBarItem(icon:ImageIcon(AssetImage('assets/images/sepha.png'),),
-             label: 'Sebha'
-         ),
-         BottomNavigationBarItem(icon:ImageIcon(AssetImage('assets/images/radio.png'))
-             ,label: 'Radio'
-         ),
-       ],
-     ),
-   ),
-   body: Taps[selectedindex],
-),
-      ],
-    );
+    return Scaffold();
   }
-  List <Widget> Taps=[
-    QuranTab(),HadethTab(),SebhaTab(),RadioTab()
-  ];
 }
